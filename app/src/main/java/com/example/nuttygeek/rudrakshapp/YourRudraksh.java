ba53f9444143f51c1ab6fb8315cd1202ad5d1c27
NuttyGeek <nuttygeeek@gmail.com>
2017-08-24 01:45:58 +0530

    list item configured

--- app/src/main/java/com/example/nuttygeek/rudrakshapp/YourRudraksh.java
+++ app/src/main/java/com/example/nuttygeek/rudrakshapp/YourRudraksh.java
@@ -22,8 +22,8 @@ import java.util.Locale;
 
 public class YourRudraksh extends AppCompatActivity {
 
-    String[] zodiacSign ={"<your zodiac sign>","Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn","Aquarius","Pisces"};
-
+    String[] zodiacSign ={"<zodiac sign>     <राशि>","Aries     मेष","Taurus     वृषभ","Gemini     मिथुन","Cancer     कर्क","Leo     सिँह",
+            "Virgo     कन्या","Libra     तुला","Scorpio     वृश्चिक","Sagittarius     धनु","Capricorn     मकर","Aquarius     कुंभ","Pisces     मीन"};
     Spinner spinner;
     Toolbar toolbar;
     CardView cardOne,cardTwo;
